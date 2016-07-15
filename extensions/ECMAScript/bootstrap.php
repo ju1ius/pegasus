0c4c7a7eaf0fa6845c4d403ad754ee17a000d6ba
ju1ius <ju1ius@laposte.net>
2016-07-15 17:51:27 +0200

    adds bootstrap file to ECMAScript extension

---
+++ extensions/ECMAScript/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of Pegasus
+ *
+ * © 2014 Jules Bernable
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use ju1ius\Pegasus\Compiler\Extension\ECMAScript\ECMAScriptExtension;
+
+return new ECMAScriptExtension();
