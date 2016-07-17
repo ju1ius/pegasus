97fc0356ea355f9d62f477d99ec0e022110226f8
ju1ius <ju1ius@laposte.net>
2016-07-18 01:29:30 +0200

    adds test for abstract expression

---
+++ tests/Pegasus/ExpressionTest.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * This file is part of Pegasus
+ *
+ * © 2014 Jules Bernable
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ju1ius\Pegasus\Tests;
+
+use ju1ius\Pegasus\Expression;
+
+/**
+ * @author ju1ius <ju1ius@laposte.net>
+ */
+class ExpressionTest extends PegasusTestCase
+{
+    public function testClone()
+    {
+        $expr = $this->getMockForAbstractClass(Expression::class);
+        $clone = clone $expr;
+        $this->assertNotSame($expr->id, $clone->id);
+    }
+
+    public function testWakeUp()
+    {
+        $expr = $this->getMockForAbstractClass(Expression::class);
+        $new = unserialize(serialize($expr));
+        $this->assertNotSame($expr->id, $new->id);
+    }
+
+    public function testIterate()
+    {
+        $expr = $this->getMockForAbstractClass(Expression::class);
+        $a = iterator_to_array($expr->iterate());
+        $this->assertSame(1, count($a));
+        $this->assertSame($expr, $a[0]);
+    }
+
+    public function testDefaultMetadata()
+    {
+        $expr = $this->getMockForAbstractClass(Expression::class);
+        $this->assertTrue($expr->isCapturing());
+        $this->assertTrue($expr->isCapturingDecidable());
+        $this->assertFalse($expr->hasVariableCaptureCount());
+    }
+}
