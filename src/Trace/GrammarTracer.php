35d7cc4f7e5b730e6e6ad7d62dffb5c0adae4b73
ju1ius <ju1ius@laposte.net>
2018-01-10 08:18:49 +0100

    prevents double-wrapping of expressions in Trace

--- src/Trace/GrammarTracer.php
+++ src/Trace/GrammarTracer.php
@@ -5,13 +5,22 @@ namespace ju1ius\Pegasus\Trace;
 
 
 use ju1ius\Pegasus\Expression;
+use ju1ius\Pegasus\Expression\Decorator\Trace;
 use ju1ius\Pegasus\Grammar\GrammarVisitor;
 
 
 final class GrammarTracer extends GrammarVisitor
 {
+    public function enterExpression(Expression $expr, ?int $index = null, bool $isLast = false)
+    {
+        if ($expr instanceof Trace) {
+            // remove the existing Trace to prevent double-wrapping
+            return $expr[0];
+        }
+    }
+
     public function leaveExpression(Expression $expr, ?int $index = null, bool $isLast = false)
     {
-        return new Expression\Decorator\Trace($expr);
+        return new Trace($expr);
     }
 }
