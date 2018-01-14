1d8cf07a2760e730f48daf886c7fb7bf1403533f
ju1ius <ju1ius@laposte.net>
2018-01-15 00:08:04 +0100

    adds tests for tracing grammar traits & parents

--- tests/Pegasus/Trace/GrammarTracerTest.php
+++ tests/Pegasus/Trace/GrammarTracerTest.php
@@ -17,8 +17,8 @@ class GrammarTracerTest extends PegasusTestCase
             ->rule('start')->oneOf()
                 ->literal('foo')
                 ->literal('bar')
-            ->getGrammar()
-            ->tracing();
+            ->getGrammar();
+        $grammar->tracing();
 
         $expected = new Trace(
             new OneOf([
@@ -37,9 +37,8 @@ class GrammarTracerTest extends PegasusTestCase
             ->rule('start')->oneOf()
                 ->literal('foo')
                 ->literal('bar')
-            ->getGrammar()
-            ->tracing(true)
-            ->tracing(false);
+            ->getGrammar();
+        $grammar->tracing(true)->tracing(false);
 
         $expected = new OneOf([
             new Literal('foo'),
@@ -55,9 +54,8 @@ class GrammarTracerTest extends PegasusTestCase
             ->rule('start')->oneOf()
                 ->literal('foo')
                 ->literal('bar')
-            ->getGrammar()
-            ->tracing()
-            ->tracing();
+            ->getGrammar();
+        $grammar->tracing()->tracing();
 
         $expected = new Trace(
             new OneOf([
@@ -68,5 +66,45 @@ class GrammarTracerTest extends PegasusTestCase
         );
 
         $this->assertExpressionEquals($expected, $grammar['start']);
+    }
+
+    public function testItTracesParents()
+    {
+        $parent = GrammarBuilder::create('parent')
+            ->rule('parent')->literal('foo')
+            ->getGrammar();
+        $child = GrammarBuilder::create('child')
+            ->rule('child')->literal('bar')
+            ->getGrammar();
+        $child->extends($parent);
+        $grandChild = GrammarBuilder::create('grandChild')
+            ->rule('grandChild')->literal('baz')
+            ->getGrammar();
+        $grandChild->extends($child);
+        $grandChild->tracing();
+
+        $expected = new Trace(new Literal('foo', 'parent'), 'parent');
+
+        $this->assertExpressionEquals($expected, $parent['parent']);
+    }
+
+    public function testItTracesTraits()
+    {
+        $grammar = GrammarBuilder::create('main')
+            ->rule('main')->literal('foo')
+            ->getGrammar();
+        $g2 = GrammarBuilder::create('g2')
+            ->rule('g2')->literal('bar')
+            ->getGrammar();
+        $g3 = GrammarBuilder::create('g3')
+            ->rule('g3')->literal('baz')
+            ->getGrammar();
+        $grammar->use($g2);
+        $g2->use($g3);
+        $grammar->tracing();
+
+        $expected = new Trace(new Literal('baz', 'g3'), 'g3');
+
+        $this->assertExpressionEquals($expected, $g3['g3']);
     }
 }
