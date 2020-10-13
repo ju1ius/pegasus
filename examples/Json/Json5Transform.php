476e191ba748cbf936aceba8fa8f83c75011b5c2
ju1ius <jules.bernable@gmail.com>
2020-10-13 10:43:14 +0200

    [wip] updates json5 example grammar as per the spec

--- examples/Json/Json5Transform.php
+++ examples/Json/Json5Transform.php
@@ -6,4 +6,24 @@ namespace ju1ius\Pegasus\Examples\Json;
 
 class Json5Transform extends JsonTransform
 {
+    protected function leave_string($node, $value)
+    {
+        return $node->attributes['groups'][2];
+    }
+
+    protected function leave_hex($node, $hex)
+    {
+        return hexdec($hex);
+    }
+
+    protected function leave_infinity($node, $inf)
+    {
+        if ($inf[0] === '-') return -INF;
+        return INF;
+    }
+
+    protected function leave_nan($node, $_)
+    {
+        return NAN;
+    }
 }
